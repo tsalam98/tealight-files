b89986e48eb6bff150a6e0b98485faf254460174
tsalam98 <tabia_salam98@hotmail.co.uk>
2015-08-18 12:18:09 +0100

    Closing logo/chessboard2.py

--- logo/chessboard2.py
+++ logo/chessboard2.py
@@ -1,1 +1,7 @@
-print "A new file!"
+def waterwheel(edges, size):
+  angle = 360 / edges
+  decoration = size / 2
+  for i in range(0, edges):
+    move(size)
+    triangle(decoration)
+    turn(angle)

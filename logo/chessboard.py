94b902d9d4aff706aa673d13cb9c8f070baa6e1c
tsalam98 <tabia_salam98@hotmail.co.uk>
2015-08-18 12:07:37 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -1,7 +1,11 @@
 from tealight.logo import (move, 
                            turn, 
                            color)
-def square(side):
+def square():
   for i in range(0,4):
     move(side)
     turn(90)
+
+   
+    
+square()

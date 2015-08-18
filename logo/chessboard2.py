cd8ad8c490fd8a99b043ec51206b3593111d3d85
tsalam98 <tabia_salam98@hotmail.co.uk>
2015-08-18 12:18:25 +0100

    Closing logo/chessboard2.py

--- logo/chessboard2.py
+++ logo/chessboard2.py
@@ -1,7 +1,4 @@
-def waterwheel(edges, size):
-  angle = 360 / edges
-  decoration = size / 2
-  for i in range(0, edges):
-    move(size)
-    triangle(decoration)
-    turn(angle)
+from tealight.logo import (move, 
+                           turn, 
+                           color)
+def square(side):

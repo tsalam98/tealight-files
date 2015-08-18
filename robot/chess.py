dbb09f51d399a4c8bcd4d3b1ce0503b4ef65e558
tsalam98 <tabia_salam98@hotmail.co.uk>
2015-08-18 14:49:58 +0100

    Running robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -6,13 +6,6 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 
-# Add your code here
-move
-turn(1)
-
-distance = 10
-for n in range(0, 32):
-  move()
 
 def eatloop():
   for n in range(0,32):

4505b5fa2796e0b4ca1bda0866827ebe6f007fbf
tsalam98 <tabia_salam98@hotmail.co.uk>
2015-08-18 11:45:09 +0100

    Running logo/demo.py

--- logo/demo.py
+++ logo/demo.py
@@ -2,7 +2,7 @@ from tealight.logo import (move,
                            turn, 
                            color)
 
-colors = ["red", "green", "blue"]
+colors = ["red", "green", "blue" , "yellow"]
 
 for i in range(0,100):
   move(i)

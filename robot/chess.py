ad7409244b24f9a574f4efc0da286603c260e560
tsalam98 <tabia_salam98@hotmail.co.uk>
2015-08-18 14:45:36 +0100

    Closing robot/chess.py

--- robot/chess.py
+++ robot/chess.py
@@ -18,14 +18,3 @@ turn(-1)
 distance = 10
 for n in range(0, 32):
   move()
-
-turn(-1)
-distance = 10
-for n in range(0, 32):
-  move()
-  
-turn(-1)
-distance = 10
-for n in range(0, 28):
-  move()
-

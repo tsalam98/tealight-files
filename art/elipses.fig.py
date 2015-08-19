f3a9f219905eebf790097f4d3743f09dfb5e8b71
tsalam98 <tabia_salam98@hotmail.co.uk>
2015-08-19 10:36:58 +0100

    Running art/elipses.fig.py

--- art/elipses.fig.py
+++ art/elipses.fig.py
@@ -1,1 +1,11 @@
-print "A new file!"
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+
+from math import sin, cos, pi
+
+def elipses(x, y, width, height):
+  
+  color(c)
+
+star(300, 300, "blue", 100, 50)
+star(600, 400, "purple", 200, 100)
+star(450, 200, "orange", 125, 30)

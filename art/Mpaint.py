a696a831823ee1d8e16fd050a9ddd6c777ef568e
tsalam98 <tabia_salam98@hotmail.co.uk>
2015-08-20 10:42:44 +0100

    Running art/Mpaint.py

---
+++ art/Mpaint.py
@@ -0,0 +1,7 @@
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+import github.alexinder.art.alexpaint
+import github.bbenny0211.art.paint
+
+colour("pink")
+def tool():
+  spot(25, 25, 50)

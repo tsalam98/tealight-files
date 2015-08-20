1d33cdc34aace4de8cd86e13ae876a03f33b993f
tsalam98 <tabia_salam98@hotmail.co.uk>
2015-08-20 15:27:10 +0100

    Running art/Mpaint.py

--- art/Mpaint.py
+++ art/Mpaint.py
@@ -8,27 +8,31 @@ y0 = 0
 rainbow = False
 
 def handle_mousemove(x,y,button):
-  global x0, y0, hue, rainbow
+  global x0, y0, hue, rainbow, c
   #print x,y,button
   if button == 'left':
     if rainbow :
       color("hsl(%d,100%%,50%%)" % hue)
+    else:
+      color(c)
     hue += 1
     line(x0,y0,x,y)
     x0 = x
     y0 = y
+  elif button == 'right':
+    c = 'white'
   else : 
     print ("press down")
     
 def handle_mousedown(x,y,button):
-  global rainbow
+  global rainbow, c
   new_color = color_click(x,y)
   if new_color != None:
     if new_color == "rainbow":
       rainbow = True
     else:
       rainbow = False
-      color(new_color)
+      c = new_color
   thick_click(x,y)
   global x0,y0
   #print x,y,button

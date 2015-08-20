b4a10d9f3845bb457d2e8af668b8cfe29e9d4c53
tsalam98 <tabia_salam98@hotmail.co.uk>
2015-08-20 10:43:14 +0100

    Closing art/Mpaint.py

--- art/Mpaint.py
+++ art/Mpaint.py
@@ -1,6 +1,4 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
-import github.alexinder.art.alexpaint
-import github.bbenny0211.art.paint
 
 colour("pink")
 def tool():

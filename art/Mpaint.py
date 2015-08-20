516bfab03f623e90f449f76aed9d658376c853fa
tsalam98 <tabia_salam98@hotmail.co.uk>
2015-08-20 10:59:01 +0100

    Running art/Mpaint.py

--- art/Mpaint.py
+++ art/Mpaint.py
@@ -1,5 +1,5 @@
 from tealight.art import (color, line, spot, circle, rectangle, box, image, text, background)
 
 color("purple")
-rectangle(50, 50, 50, 50)
+rectangle(75, 75, 50, 50)
 tool = spot(100, 100, 10)

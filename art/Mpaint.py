e190f7425dc5fa0de9f65bcbd456fa35f4292e28
tsalam98 <tabia_salam98@hotmail.co.uk>
2015-08-20 13:53:38 +0100

    Running art/Mpaint.py

--- art/Mpaint.py
+++ art/Mpaint.py
@@ -2,12 +2,12 @@ from tealight.art import (color, line, spot, circle, rectangle, box, image, text
 
 
 tool = 10
-brushcolor = "green"
+brushcolor = "purple"
 
 def handle_mousemove(x,y,button):
   print x,y,button
   if button == 'left':
     color(brushcolor)
-    line(x,y, tool, 5)
+    line(x,y, tool, 6)
   else : 
     print ("press down")

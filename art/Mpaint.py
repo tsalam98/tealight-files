be9ac5b8e34f172e03ae18bca1309b55deab5fc3
tsalam98 <tabia_salam98@hotmail.co.uk>
2015-08-20 14:08:37 +0100

    Running art/Mpaint.py

--- art/Mpaint.py
+++ art/Mpaint.py
@@ -7,6 +7,17 @@ brushcolor = "purple"
 x0 = 0
 y0 = 0
 
+def handle_mousemove(x,y,button):
+  global x0, y0
+  print x,y,button
+  if button == 'left':
+    color(brushcolor)
+    line(x0,y0,x,y)
+    x0 = x
+    y0 = y
+  else : 
+    print ("press down")
+
     
 def handle_mousedown(x,y,button):
   global x0,y0

6ae58b7048703d481034db8f7b249c0663a6530a
tsalam98 <tabia_salam98@hotmail.co.uk>
2015-08-21 09:38:01 +0100

    Running art/Mpaint.py

--- art/Mpaint.py
+++ art/Mpaint.py
@@ -3,7 +3,7 @@ from github.alexinder.art.alexpaint import *
 from github.bbenny0211.art.paint import *
 
 rectangle (15, 780, 150, 35)
-box (25, 782.5, 125, 30)
+box (25, 78, 125, 25)
 hue = 0
 x0 = 0
 y0 = 0
